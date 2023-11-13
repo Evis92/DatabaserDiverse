aeb1c32780c9a85488e8e75b7ca8d5743b8a5a6d
Evis92 <143110072+Evis92@users.noreply.github.com>
2023-11-13 15:55:10 +0100

    Add files via upload

---
+++ EvaLottaIntro.sql
@@ -0,0 +1,26 @@
+--CREATE DATABASE	KlassenDb;
+
+USE KlassenDB
+
+--CREATE TABLE KlassenTbl(
+--Id Int,
+--Förnamn nvarchar(max) NOT NULL,
+--Efternamn nvarchar(max) NOT NULL,
+--Ålder Int,
+--Hemort nvarchar(max) NOT NULL
+--PRIMARY KEY(Id)
+--);
+
+--INSERT INTO KlassenTbl (Id, Förnamn, Efternamn, Ålder, Hemort)
+--VALUES (1, 'Eva-Lotta', 'Karlsson', 31, 'Göteborg');
+
+--INSERT INTO KlassenTbl (Id, Förnamn, Efternamn, Ålder, Hemort)
+--VALUES (2, 'Geweria', 'Negash', 20, 'Göteborg');
+
+--INSERT INTO KlassenTbl (Id, Förnamn, Efternamn, Ålder, Hemort)
+--VALUES (3, 'Rasmus', 'Olofsson', 28, 'Kungälv');
+
+--INSERT INTO KlassenTbl (Id, Förnamn, Efternamn, Ålder, Hemort)
+--VALUES (4, 'Anders', 'Öberg', 32, 'Göteborg');
+
+--SELECT * FROM KlassenTbl;
